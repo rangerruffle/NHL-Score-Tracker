61276b42f69b2570db2d7e1ccdd108bd90328f93
rufflezberg <erickson.russell.j@gmail.com>
2018-01-26 11:24:48 -0600

    Work from work

--- NHL Extension/popup.js
+++ NHL Extension/popup.js
@@ -1,16 +1,26 @@
 var teams = [ "Avalanche", "Blackhawks", "Blue Jackets", "Blues", "Bruins", "Canadiens", "Canucks", "Capitals", "Coyotes", "Devils", "Ducks", "Flames", "Flyers", "Golden Knights", "Hurricanes", "Islanders", "Jets", "Kings", "Lightning", "Maple Leafs", "Oilers", "Panthers", "Penguins", "Predators", "Rangers", "Red Wings", "Sabres", "Senators", "Sharks", "Stars", "Wild" ];
+var teamIds = { "Avalanche": 21, "Blackhawks": 16, "Blue Jackets": 29, "Blues": 19, "Bruins": 6, "Canadiens": 8, "Canucks": 23, "Capitals": 15, "Coyotes": 53, "Devils": 1, "Ducks": 24, "Flames": 20, "Flyers": 4, "Golden Knights": 54, "Hurricanes": 12, "Islanders": 2, "Jets": 52, "Kings": 26, "Lightning": 14, "Maple Leafs": 10, "Oilers": 22, "Panthers": 13, "Penguins": 5, "Predators": 18, "Rangers": 3, "Red Wings": 17, "Sabres": 7, "Senators": 9, "Sharks": 28, "Stars": 25, "Wild": 30 };
 
+var awayTeamIcon = "logos/nhl.png";
+var homeTeamIcon = "logos/nhl.png";
 var teamName = "";
+var teamIcon = "logos/nhl.png";
 var teamId = "";
 var timeZone = "US/Central";
 var todayYear = "";
 var todayMonth = "";
 var todayDay = "";
 
+const rink = document.getElementById('rink');
+const preview = document.getElementById('preview');
+const gameFinal = document.getElementById('gameFinal');
+const standings = document.getElementById('standings');
+
 chrome.storage.sync.get([ 'trackedTeamName','trackedTimeZone' ], function(result) {
 	if (result.trackedTeamName) {
 		teamName = result.trackedTeamName;
 		teamIcon = "logos/" + result.trackedTeamName + ".png";
+		teamId = teamIds[teamName];
 	}
 	
 	if (result.trackedTimeZone) {
@@ -55,149 +65,149 @@ function updateGameData(yyyy, mm, dd) {
 			if (scheduleInfo.dates[0]) {
 				if (scheduleInfo.dates[0].games[0]) {
 					currentGameId = scheduleInfo.dates[0].games[0].gamePk;
-					var dateTime = new Date(scheduleInfo.dates[0].games[0].gameDate);
-					localGameTime = getTimeZoneAdjustedTime(dateTime);
 					gameLiveLink = scheduleInfo.dates[0].games[0].link;
+					hideShowElements(rink);
 				} else {
 					currentGameId = false;
-					chrome.browserAction.setTitle({title: "No " + teamName + " game today."});
-					chrome.browserAction.setBadgeText({text: ""});
-					drawLogo(teamIcon, false);
+					drawAwayLogo("logos/nhl.png", false);
+					drawHomeLogo("logos/nhl.png", false);
+					hideShowElements(standings);
 				}
 			} else {
 				currentGameId = false;
-				chrome.browserAction.setTitle({title: "No " + teamName + " game today."});
-				chrome.browserAction.setBadgeText({text: ""});
-				drawLogo(teamIcon, false);
+				drawAwayLogo("logos/nhl.png", false);
+				drawHomeLogo("logos/nhl.png", false);
+				hideShowElements(standings);
 			}
 		}
 	}
 
 	setTimeout(function() {
-			if (currentGameId) {
-				var gameXmlHttp = new XMLHttpRequest();
-				gameXmlHttp.open("GET", "https://statsapi.web.nhl.com/" + gameLiveLink);
-				gameXmlHttp.send(null);
-				gameXmlHttp.onreadystatechange = function() {
-					if (gameXmlHttp.readyState == 4 && gameXmlHttp.status == 200) {
-						var gameInfo = JSON.parse(gameXmlHttp.responseText);
-						var game = gameInfo.gameData;
-						teamIsHome = game.teams.home.id === teamId;
-						awayTeamInitial = game.teams.away.abbreviation.toLowerCase();
-						homeTeamInitial = game.teams.home.abbreviation.toLowerCase();
+		if (currentGameId) {
+			var gameXmlHttp = new XMLHttpRequest();
+			gameXmlHttp.open("GET", "https://statsapi.web.nhl.com/" + gameLiveLink);
+			gameXmlHttp.send(null);
+			gameXmlHttp.onreadystatechange = function() {
+				if (gameXmlHttp.readyState == 4 && gameXmlHttp.status == 200) {
+					var gameInfo = JSON.parse(gameXmlHttp.responseText);
+					var game = gameInfo.gameData;
+					var teamIsHome = game.teams.home.id === teamId;
+					awayTeamInitial = game.teams.away.abbreviation.toLowerCase();
+					homeTeamInitial = game.teams.home.abbreviation.toLowerCase();
+					var gameToday = currentGameId != false;
+					
+					if (gameToday) {
+						const awayScoreElement = document.getElementById('awayScore');
+						const timeLeftElement = document.getElementById('time');
+						const homeScoreElement = document.getElementById('homeScore');
+
+						var awayScore = gameInfo.liveData.linescore.teams.away.goals;
+						var homeScore = gameInfo.liveData.linescore.teams.home.goals;
+						var otherTeamName = "other team";
+						var venue = game.teams.home.venue.name;
+						otherTeamName = teamIsHome ? game.teams.away.teamName : game.teams.home.teamName;
 						
-						var tagText = "No " + teamName + " game today.";
-						var badgeText = "";
-						var icon = teamIcon;
-						var gameToday = currentGameId != false;
-						
-						if (gameToday) {
-							var awayScore = gameInfo.liveData.linescore.teams.away.goals;
-							var homeScore = gameInfo.liveData.linescore.teams.home.goals;
-							var otherTeamName = "other team";
-							var goalie = "";
-							var venue = game.teams.home.venue.name;
-							otherTeamName = teamIsHome ? game.teams.away.teamName : game.teams.home.teamName;
-							
-							if (game.status.abstractGameState == "Preview") {
-								// Hide the rink and display some preview stats for the game.
-								tagText = teamName + " vs " + otherTeamName + " at " + venue + ". Puck drops at " + localGameTime + ".";
-								badgeText = localGameTime.substring(0, localGameTime.length - 2);
-								currentlyPreGame = true;
-							} else if (game.status.abstractGameState == "Final") {
-								// Hide the rink and display some stats from the game, including outcome.
-								var gameResult;
-								var teamScore = teamIsHome ? homeScore : awayScore;
-								var otherTeamScore = teamIsHome ? awayScore : homeScore;
-								if (otherTeamScore > teamScore) {
-									gameResult = "lost to";
-									icon = lossIcon;
-								} else {
-									gameResult = "beat";
-									icon = winIcon;
-								}
-								
-								tagText = "The " + teamName + " " + gameResult + " the " + otherTeamName + " " + teamScore + "-" + otherTeamScore + " at " + venue;
-								badgeText = awayScore + "-" + homeScore;
-								
-								if (gameTimeDataRefreshTimer) {
-									window.clearInterval(gameTimeDataRefreshTimer);
-									gameTimeDataRefreshTimer = false;
-								}
-								currentlyPreGame = false;
-							} else if (game.status.abstractGameState == "Live") {
-								// Update data in the rink, scores, and time.
-								var scoreStatus;
-								var teamScore = teamIsHome ? homeScore : awayScore;
-								var otherTeamScore = teamIsHome ? awayScore : homeScore;
-								if (teamScore > otherTeamScore) {
-									scoreStatus = "leading";
-								} else if (teamScore < otherTeamScore) {
-									scoreStatus = "trailing"
-								} else if (awayScore == homeScore) {
-									scoreStatus = "tied with";
-								}
-								
-								var period = gameInfo.liveData.linescore.currentPeriod;
-								var isShootout = period === 5;
-								switch (period) {
-									case 1:
-										period = "the 1st period";
-										break;
-									case 2:
-										period = "the 2nd period";
-										break;
-									case 3:
-										period = "the 3rd period";
-										break;
-									case 4:
-										period = "overtime";
-										break;
-									case 5:
-										period = "the shootout";
-										break;
-								}
-								
-								if (gameInfo.liveData.linescore.currentPeriodTimeRemaining === "END") {
-									tagText = "The " + teamName + " are " + scoreStatus + " the " + otherTeamName + " " + teamScore + "-" + otherTeamScore + " at the end of " + period + ".";
-								} else {
-									if (isShootout) {
-										home = gameInfo.liveData.linescore.shootoutInfo.home.scores;
-										away = gameInfo.liveData.linescore.shootoutInfo.away.scores;
-										if (teamIsHome) {
-											teamScore = home;
-											otherTeamScore = away;
-										} else {
-											teamScore = away;
-											otherTeamScore = home;
-										}
-										tagText = "The " + teamName + " are " + scoreStatus + " the " + otherTeamName + " " + teamScore + "-" + otherTeamScore + " in " + period;
-									} else {
-										tagText = "The " + teamName + " are " + scoreStatus + " the " + otherTeamName + " " + teamScore + "-" + otherTeamScore + " in " + period + " with " + gameInfo.liveData.linescore.currentPeriodTimeRemaining + " remaining";
-									}
-								}
-								
-								badgeText = awayScore + "-" + homeScore;
-								startInGameDataUpdateTimerIfNeeded();
-								currentlyPreGame = false;
-							} else {
-								badgeText = "TBD";
-								tagText = "The status of the game could not be determined at this time. Please try reselecting your team. If that does not work, please notify the developer or look at NHL.com as their servers may be down.";
+						if (game.status.abstractGameState == "Preview") {
+							// Hide the rink and display some preview stats for the game.
+							hideShowElements(preview);
+							currentlyPreGame = true;
+						} else if (game.status.abstractGameState == "Final") {
+							// Hide the rink and display some stats from the game, including outcome.
+							hideShowElements(gameFinal);
+							if (gameTimeDataRefreshTimer) {
+								window.clearInterval(gameTimeDataRefreshTimer);
+								gameTimeDataRefreshTimer = false;
 							}
+							currentlyPreGame = false;
+						} else if (game.status.abstractGameState == "Live") {
+							// Update data in the rink, scores, and time.
+							hideShowElements(rink);
+							var period = gameInfo.liveData.linescore.currentPeriod;
+							var isShootout = period === 5;
+							switch (period) {
+								case 1:
+									period = "the 1st period";
+									break;
+								case 2:
+									period = "the 2nd period";
+									break;
+								case 3:
+									period = "the 3rd period";
+									break;
+								case 4:
+									period = "overtime";
+									break;
+								case 5:
+									period = "the shootout";
+									break;
+							}
+							
+							if (gameInfo.liveData.linescore.currentPeriodTimeRemaining === "END") {
+								// Time equals END, hide rink, and show final layout with the stats from the period that just ended.
+								hideShowElements(gameFinal);
+							} else {
+								if (isShootout) {
+									home = gameInfo.liveData.linescore.shootoutInfo.home.scores;
+									away = gameInfo.liveData.linescore.shootoutInfo.away.scores;
+									if (teamIsHome) {
+										teamScore = home;
+										otherTeamScore = away;
+									} else {
+										teamScore = away;
+										otherTeamScore = home;
+									}
+									tagText = "The " + teamName + " are " + scoreStatus + " the " + otherTeamName + " " + teamScore + "-" + otherTeamScore + " in " + period;
+								} else {
+									tagText = "The " + teamName + " are " + scoreStatus + " the " + otherTeamName + " " + teamScore + "-" + otherTeamScore + " in " + period + " with " + gameInfo.liveData.linescore.currentPeriodTimeRemaining + " remaining";
+								}
+							}
+							startInGameDataUpdateTimerIfNeeded();
+							currentlyPreGame = false;
+						} else {
+							hideShowElements(standings);
 						}
-						
-						if (tagText) {
-							chrome.browserAction.setTitle({title: tagText});
-						}
-						if (badgeText !== false) {
-							chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 255] });
-							chrome.browserAction.setBadgeText({text: badgeText});
-						}
-						drawLogo(icon, gameToday);
 					}
 				}
 			}
-		},
-		250
-	);
+		}
+		
+		drawAwayLogo(awayTeamIcon);
+		drawHomeLogo(homeTeamIcon);
+	}, 250);
+}
+
+function drawAwayLogo(icon) {
+	const awayLogo = document.getElementById('awayImage');
+	awayLogo.src = icon;
+}
+
+function drawHomeLogo(icon) {
+	const homeLogo = document.getElementById('homeImage');
+	homeLogo.src = icon;
+}
+
+function hideShowElements(elementToShow) {
+	if (rink === elementToShow && !elementToShow.classList.contains('hidden')) {
+		rink.classList.add('hidden');
+	} else {
+		rink.classList.remove('hidden');
+	}
+
+	if (preview === elementToShow && !elementToShow.classList.contains('hidden')) {
+		preview.classList.add('hidden');
+	} else {
+		preview.classList.remove('hidden');
+	}
+
+	if (gameFinal === elementToShow && !elementToShow.classList.contains('hidden')) {
+		gameFinal.classList.add('hidden');
+	} else {
+		gameFinal.classList.remove('hidden');
+	}
+
+	if (standings === elementToShow && !elementToShow.classList.contains('hidden')) {
+		standings.classList.add('hidden');
+	} else {
+		standings.classList.remove('hidden');
+	}
 }
